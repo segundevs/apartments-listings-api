8c01d7bc672e32c9a4431d9a3f6f6875a121bb5b
segundevs <tushbash@gmail.com>
2021-11-11 11:08:45 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const apartmentRoute = require('./routes/apartments');
+
+const app = express();
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log('MongoDb connected'))
+.catch(err => console.log(err));
+
+
+app.listen(port, (req, res) => {
+  console.log('listening on port 8080')
+})
+
+app.use('/api/apartments', apartmentRoute);
