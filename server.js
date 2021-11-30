570f27866bd64581048e3559b41c73972a1a1aac
segundevs <tushbash@gmail.com>
2021-11-30 02:01:18 -0800

    added access headers middleware

--- server.js
+++ server.js
@@ -11,23 +11,6 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://apartmentshub.netlify.app/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
 const port = process.env.PORT || 8080;
 
 mongoose
