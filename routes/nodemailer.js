d6e799e25cc0a16caeb3f93fad0198df7650a634
segundevs <tushbash@gmail.com>
2021-11-30 02:18:01 -0800

    access headers middleware

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -1,5 +1,25 @@
 const router = require("express").Router();
+const express = require("express");
 const nodemailerController = require("../controllers/nodemailerController");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://apartmentshub.netlify.app/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 router.post("/mail", nodemailerController.sendMail);
 
