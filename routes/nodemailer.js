34d58d796b737c00ef0aa6b32d3611fcbfce549e
segundevs <tushbash@gmail.com>
2021-11-30 02:30:37 -0800

    fixed cors issue

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -4,23 +4,25 @@ const nodemailerController = require("../controllers/nodemailerController");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://apartmentshub.netlify.app/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
-router.post("/mail", nodemailerController.sendMail);
+router.post(
+  "/mail",
+  app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://apartmentshub.netlify.app/"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  }),
+  nodemailerController.sendMail
+);
 
 module.exports = router;
