577edd579b355039f10e0b87bb2350d0fd15fd01
segundevs <tushbash@gmail.com>
2021-11-30 01:55:46 -0800

    added access to live url

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://apartmentshub.netlify.app/"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
